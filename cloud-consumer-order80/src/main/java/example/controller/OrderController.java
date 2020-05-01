9acc75e7e1c81cf5bf70bcb0ff5882162fa847d0
bkn <bbb_gbl@sf.com>
2020-05-01 15:35:24 +0800

    common module

--- cloud-consumer-order80/src/main/java/example/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/example/controller/OrderController.java
@@ -1,7 +1,7 @@
 package example.controller;
 
-import example.entities.CommenResult;
-import example.entities.Payment;
+import com.example.entities.CommenResult;
+import com.example.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
