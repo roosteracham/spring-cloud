e8122177ab2dae90f2e4cb55e55aa840dd284bd1
bkn <bbb_gbl@sf.com>
2020-05-03 16:40:03 +0800

    gateway 负载均衡， 服务id，大小写不敏感

--- cloud-gateway-demo/src/main/java/com/example/config/GatewayRoute.java
+++ cloud-gateway-demo/src/main/java/com/example/config/GatewayRoute.java
@@ -1,6 +1,5 @@
 package com.example.config;
 
-import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
