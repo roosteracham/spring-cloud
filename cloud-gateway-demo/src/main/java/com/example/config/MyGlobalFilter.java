44465dca5769e4da7b67333336cb0b2a644a61a4
bkn <bbb_gbl@sf.com>
2020-05-03 17:02:23 +0800

    gateway 自定义过滤器

---
+++ cloud-gateway-demo/src/main/java/com/example/config/MyGlobalFilter.java
@@ -0,0 +1,34 @@
+package com.example.config;
+
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class MyGlobalFilter implements GlobalFilter, Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+
+        MultiValueMap<String, String> params = exchange.getRequest().getQueryParams();
+        log.info(JSON.toJSONString(exchange.getResponse().getStatusCode()));
+        exchange.getResponse().setStatusCode(HttpStatus.ACCEPTED);
+        if (params.getFirst("name") == null) {
+            exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+            return exchange.getResponse().setComplete();
+        }
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
