3909d664d08c924d976661b773a5d59495923e3f
bkn <bbb_gbl@sf.com>
2020-05-13 23:12:22 +0800

    consumer  namespace group profile

--- nacos-config-client/src/main/java/com/example/NacosConfigApplication.java
+++ nacos-config-client/src/main/java/com/example/NacosConfigApplication.java
@@ -5,14 +5,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import java.util.concurrent.TimeUnit;
+
 @EnableDiscoveryClient
 @SpringBootApplication
 public class NacosConfigApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ConfigurableApplicationContext applicationContext = SpringApplication.run(NacosConfigApplication.class, args);
-        String userName = applicationContext.getEnvironment().getProperty("user.name");
-        String userAge = applicationContext.getEnvironment().getProperty("user.age");
-        System.err.println("user name :" +userName+"; age: "+userAge);
+        while(true) {
+            String userName = applicationContext.getEnvironment().getProperty("user.name");
+            String userAge = applicationContext.getEnvironment().getProperty("user.age");
+            //Get the current deployment environment
+            String currentEnv = applicationContext.getEnvironment().getProperty("current.env");
+            System.err.println("in "+currentEnv+" enviroment; "+"user name :" + userName + "; age: " + userAge);
+            TimeUnit.SECONDS.sleep(5);
+        }
     }
 }
